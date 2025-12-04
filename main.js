53d7695322a1d57f384965509c3e4ac7c6f42f5b
Hakeem <ahmedhakemali99@gmail.com>
2025-12-04 05:39:46 +0200

    third commit

--- main.js
+++ main.js
@@ -10,27 +10,33 @@ let tmp;
 let newToDo;
 if (localStorage.ToDo != null) {
     newToDo = JSON.parse(localStorage.ToDo);
-}else {
+} else {
     newToDo = [];
 }
 
 
 submit.onclick = function () {
+    if (title.value.trim() === "") {
+        alert("Please enter a title for the task.");
+        return;
+    }
+
     let newObj = {
         title: title.value,
         date: date.value,
         description: description.value,
+        status: "pending"
     }
 
-    if(mood === "create"){
+    if (mood === "create") {
         newToDo.push(newObj);
         localStorage.setItem("ToDo", JSON.stringify(newToDo));
-    } else{
-        newToDo[tmp] = newObj; 
+    } else {
+        newToDo[tmp] = newObj;
         mood = "create";
         submit.innerHTML = "Create";
     }
-    
+
 
     clearData();
     showData();
@@ -47,15 +53,16 @@ function clearData() {
 // Read
 function showData() {
     table = "";
-    for (let i = 0; i < newToDo.length; i++){
+    for (let i = 0; i < newToDo.length; i++) {
         table += `
         <tr>
-        <td>${i}</td>
+        <td>${i + 1}</td>
         <td>${newToDo[i].title}</td>
-        <td>${newToDo[i].date}</td>
         <td>${newToDo[i].description}</td>
+        <td>${newToDo[i].date}</td>
         <td><button onclick = "updateData(${i})">Edit</button></td>
-        <td><button onclick = "deleteData(${i})">Delete</button></td>
+        <td><button id="mybutton" onclick = "toggleStatus(${i})">${newToDo[i].status}</button></td>
+        <td><button class="delete" onclick = "deleteData(${i})">Delete</button></td>
         </tr>
         `
     }
@@ -63,17 +70,33 @@ function showData() {
 }
 showData();
 
+// Toggle Status
+function toggleStatus(i) {
+    if (newToDo[i].status === "pending") {
+        newToDo[i].status = "completed";
+    } else {
+        newToDo[i].status = "pending";
+    }
+    localStorage.setItem("ToDo", JSON.stringify(newToDo));
+    showData();
+}
+
+
+
 
 // Delete
-function deleteData(i){
-    newToDo.splice(i, 1);
-    localStorage.ToDo = JSON.stringify(newToDo);
+function deleteData(i) {
+    let msg = confirm("Are you sure you want to delete this task!");
+    if (msg) {
+        newToDo.splice(i, 1);
+        localStorage.ToDo = JSON.stringify(newToDo);
+    }
     showData();
 }
 
 
 // Update
-function updateData(i){
+function updateData(i) {
     title.value = newToDo[i].title;
     date.value = newToDo[i].date;
     description.value = newToDo[i].description;
@@ -87,19 +110,20 @@ function updateData(i){
 }
 
 
-// Search 
-function searchData(value){
+// Search
+function searchData(value) {
     let table = "";
-    for (let i = 0; i < newToDo.length; i++){
-        if (newToDo[i].title.includes(value)){
+    for (let i = 0; i < newToDo.length; i++) {
+        if (newToDo[i].title.includes(value)) {
             table += `
             <tr>
-            <td>${i}</td>
+            <td>${i + 1}</td>
             <td>${newToDo[i].title}</td>
-            <td>${newToDo[i].date}</td>
             <td>${newToDo[i].description}</td>
+            <td>${newToDo[i].date}</td>
             <td><button onclick = "updateData(${i})">Edit</button></td>
-            <td><button onclick = "deleteData(${i})">Delete</button></td>
+            <td><button onclick = "toggleStatus(${i})">${newToDo[i].status}</button></td>
+            <td><button  style="background-color:red;" onclick = "deleteData(${i})">Delete</button></td>
             </tr>
             `
         }
